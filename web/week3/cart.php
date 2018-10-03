447852244f31aa31eaebe27d4df83b10d0350e65
joerockclimber <joerockclimber@gmail.com>
2018-10-03 16:48:40 -0600

    submit

--- web/week3/cart.php
+++ web/week3/cart.php
@@ -34,10 +34,9 @@ $places = $_POST["places"];
                     {
                         $place_clean = htmlspecialchars($place);
                         if($place_clean != null && $place_clean >= 1){
-                            echo "<tr><td>$place_clean</td> <td>$names[$n]</td>" . "<td>" . $place_clean * 15.00 . '$</td></tr>';
+                            echo "<tr><td>$place_clean</td> <td>$names[$n]</td>" . "<td>" . $place_clean * 15.00 . "$</td></tr>";
                             
                             $_SESSION[$n] = array($names[$n],$place);
-                            echo '<meta http-equiv="refresh" content="0"">';
                         }
                         $n++;
                     }
