96b7fb639ba8fbb4a542eda88a63db4691f51b55
joerockclimber <joerockclimber@gmail.com>
2018-10-03 09:11:12 -0600

    added session array to cart

--- web/week3/cart.php
+++ web/week3/cart.php
@@ -1,7 +1,10 @@
 <?php 
+session_start();
+session_unset();
 $places = $_POST["places"];
 print_r ($_POST);
 ?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -9,6 +12,7 @@ print_r ($_POST);
     </head>
     <body>
         <ul>
+            
             <?
             $n = 0;
             $names = array(0 => 'North Amaerica', 1 => 'South America', 2 => Asia, 3 => 'Europe', 4 => 'Africa', 5 => 'Australia', 6 => 'Antarctica');
@@ -17,8 +21,10 @@ print_r ($_POST);
                 $place_clean = htmlspecialchars($place);
                 if($place_clean != null && $place_clean >= 1){
                 echo "<li><p>$place_clean $names[$n] </p></li>";
+                $_SESSION[$n] = array(names[$n],$place);
                 }
                 $n++;
+                print_r($_SESSION);
             }
             ?>		
 
