3106994d51da2593957044a01feb621242a80763
joerockclimber <joerockclimber@gmail.com>
2018-10-03 13:26:17 -0600

    center cart

--- web/week3/cart.php
+++ web/week3/cart.php
@@ -28,7 +28,7 @@ $places = $_POST["places"];
                     {
                         $place_clean = htmlspecialchars($place);
                         if($place_clean != null && $place_clean >= 1){
-                            echo "<li><p>$place_clean $names[$n] $place_clean * 15.00 </p></li>";
+                            echo "<li><p>" . $place_clean $names[$n] $place_clean * 15.00 . "$ </p></li>";
                             $_SESSION[$n] = array($names[$n],$place);
                         }
                         $n++;
