9d43eaf2ee1b8a9f4cf23982d1b3800eea31ff2f
joerockclimber <joerockclimber@gmail.com>
2018-10-02 19:16:11 -0600

    if statement in cart

--- web/week3/cart.php
+++ web/week3/cart.php
@@ -13,7 +13,7 @@ $places = $_POST["places"];
             {
                 $place_clean = htmlspecialchars($place);
                 if($place_clean != null && $place_clean >= 1){
-                echo "<li><p>$place_clean</p></li>";
+                echo "<li><p>$place_clean " . key($place) . " </p></li>";
                 }
             }
             ?>		
