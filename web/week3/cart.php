1b3c53e7254414e0f52ee2749b1db56a4fc62a8b
joerockclimber <joerockclimber@gmail.com>
2018-10-03 21:05:49 -0600

    submit

--- web/week3/cart.php
+++ web/week3/cart.php
@@ -1,6 +1,7 @@
 <?php 
 session_start();
 $places = $_POST["places"];
+$price = 15;
 ?>
 
 <!DOCTYPE html>
@@ -33,7 +34,7 @@ $places = $_POST["places"];
                         {
                             $place_clean = htmlspecialchars($place);
                             if($place_clean != null && $place_clean >= 1){
-                                echo "<tr><td>$place_clean</td> <td>$names[$n]</td>" . "<td>" . $place_clean * 15.00 . "$</td><td><input name=\"#$n\" type=\"submit\" value=\"Submit\"></td></tr>";
+                                echo "<tr><td>$place_clean</td> <td>$names[$n]</td>" . "<td>" . $place_clean * $price . "$</td><td><input name=\"#$n\" type=\"submit\" value=\"Submit\"></td></tr>";
                                 $_SESSION[$n] = array($names[$n],$place);
                             }
                             $n++;
