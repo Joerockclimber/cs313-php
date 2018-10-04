01cf6d98fd6cea5b14b19a52e089a25728d237a8
joerockclimber <joerockclimber@gmail.com>
2018-10-03 19:47:30 -0600

    submit

--- web/week3/cart.php
+++ web/week3/cart.php
@@ -19,35 +19,37 @@ $places = $_POST["places"];
         </div>
         <div class="wrapper">
             <div class="cart">
-                <center><h1>Cart</h1></center>
-                <table>
-                    <tr>
-                        <th>Quantity</th>
-                        <th>Map</th>
-                        <th>Price</th>
-                        <th></th>
-                    </tr>
-                    <?php
-                    $n = 0;
-                    $names = array(0 => 'North Amaerica', 1 => 'South America', 2 => Asia, 3 => 'Europe', 4 => 'Africa', 5 => 'Australia', 6 => 'Antarctica');
-                    foreach ($places as $place)
-                    {
-                        $place_clean = htmlspecialchars($place);
-                        if($place_clean != null && $place_clean >= 1){
-                            echo "<tr><td>$place_clean</td> <td>$names[$n]</td>" . "<td>" . $place_clean * 15.00 . "$</td><td><input name=\"#$n\" type=\"submit\" value=\"Submit\"></td></tr>";
+                <form method="post">
+                    <center><h1>Cart</h1></center>
+                    <table>
+                        <tr>
+                            <th>Quantity</th>
+                            <th>Map</th>
+                            <th>Price</th>
+                            <th></th>
+                        </tr>
+                        <?php
+                        $n = 0;
+                        $names = array(0 => 'North Amaerica', 1 => 'South America', 2 => Asia, 3 => 'Europe', 4 => 'Africa', 5 => 'Australia', 6 => 'Antarctica');
+                        foreach ($places as $place)
+                        {
+                            $place_clean = htmlspecialchars($place);
+                            if($place_clean != null && $place_clean >= 1){
+                                echo "<tr><td>$place_clean</td> <td>$names[$n]</td>" . "<td>" . $place_clean * 15.00 . "$</td><td><input name=\"#$n\" type=\"submit\" value=\"Submit\"></td></tr>";
 
-                            $_SESSION[$n] = array($names[$n],$place);
+                                $_SESSION[$n] = array($names[$n],$place);
+                            }
+                            $n++;
                         }
-                        $n++;
-                    }
+print_r($_POST);
+                        if(isset($_POST['submit']))
+                        {
 
-                    if(isset($_POST['submit']))
-                    {
-                        
-                    } 
-                    ?>		
+                        } 
+                        ?>		
 
-                </table>
+                    </table>
+                </form>
             </div>
         </div>
     </body>
