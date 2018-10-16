8ecd3fa74270f573933a26f9832638f8c4830912
joerockclimber <joerockclimber@gmail.com>
2018-10-16 15:24:06 -0600

    Jake is super cool

---
+++ web/week4/climbing.php
@@ -0,0 +1,52 @@
+<?php
+
+try
+{
+    $dbUrl = getenv('DATABASE_URL');
+
+    $dbOpts = parse_url($dbUrl);
+
+    $dbHost = $dbOpts["host"];
+    $dbPort = $dbOpts["port"];
+    $dbUser = $dbOpts["user"];
+    $dbPassword = $dbOpts["pass"];
+    $dbName = ltrim($dbOpts["path"],'/');
+
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+    echo 'Error!: ' . $ex->getMessage();
+    die();
+} 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Main Page</title>
+        <link rel="stylesheet" type="text/css" href="index.css">
+    </head>
+    <body>
+        <div class="navbar">
+            <a href="index.php">Home</a>
+            <a href="assignments.php">Assigments</a>
+            <div id="time"><?php echo date("h:i:sa Y/m/d");?></div>
+        </div>
+        <div class="wrapper">
+            <div>
+                <?
+                $db->query('SELECT name FROM climber')
+                {
+                    echo 'Climber: ' . $db['name'];
+                    echo '<br/>';
+                }
+                ?>
+                Trip:
+                Climb:
+            </div>
+        </div>
+    </body>
+</html>
