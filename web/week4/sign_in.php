aa2f056d03511785cdbf5fd39924d6e58d7d678e
joerockclimber <joerockclimber@gmail.com>
2018-10-18 14:48:57 -0600

    Jake is super cool

---
+++ web/week4/sign_in.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Main Page</title>
+        <link rel="stylesheet" type="text/css" href="index.css">
+    </head>
+    <body>
+        <div class="navbar">
+            <a href="../index.php">Home</a>
+            <a href="../assignments.php">Assigments</a>
+            <div id="time"><?php echo date("h:i:sa Y/m/d");?></div>
+        </div>
+        <div class="sign_in">
+            <center>
+                <form method="POST" action="climbing.php">
+                    <label for="sign_in">Sign in</label>
+                    <input type="text" placeholder="Sign in" id="sign_in" name="sign_in">
+                    <input type="submit" value="Confirm" id ="confirm">
+                </form>
+            </center>
+        </div>
+    </body>
+</html>
