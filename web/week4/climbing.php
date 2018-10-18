059ee915dc751f0b11686cd7ac5bdc44f9b1d7dc
joerockclimber <joerockclimber@gmail.com>
2018-10-18 11:12:31 -0600

    Jake is super cool

--- web/week4/climbing.php
+++ web/week4/climbing.php
@@ -47,9 +47,9 @@ catch (PDOException $ex)
                 
                 <?
                 
-                $result = $db->query('SELECT trip.date, trip.location, climb.grade, climb.climb_name FROM trip JOIN climb on trip.trip_id = climb.trip_id ORDER BY trip.trip_id');
+                $db->query('SELECT trip.date, trip.location, climb.grade, climb.climb_name FROM trip JOIN climb on trip.trip_id = climb.trip_id ORDER BY trip.trip_id');
                  
-                var_dump($result);
+                var_dump($db);
                 
         
                 
