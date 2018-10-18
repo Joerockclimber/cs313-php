c8447cf3d1df157ee8123e4e582a95911aa72ba9
joerockclimber <joerockclimber@gmail.com>
2018-10-18 11:39:42 -0600

    Jake is super cool

--- web/week4/climbing.php
+++ web/week4/climbing.php
@@ -53,10 +53,10 @@ catch (PDOException $ex)
                     echo '<br/>';
                     $trip_id = $row['trip_id'];
 
-                    $stmt = $db->prepare('SELECT name, grade FROM climb where trip_id = $trip_id ');
-                    echo 'yes';
-                    $stmt->execute(array(':id' => $trip_id));
+                    $stmt = $db->prepare('SELECT name, grade FROM climb where trip_id = :id');
                     
+                    $stmt->execute(array(':id' => $trip_id));
+                    echo 'yes';
                     while ($row2 = $stmt->fetchAll(PDO::FETCH_ASSOC))
                     {
                         echo 'Climb: ' . $row2['name'] . ' grade: ' . $row2['grade']; 
