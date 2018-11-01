012cc6e620f4270be36e1b31a21975f3b88eb8b0
joerockclimber <joerockclimber@gmail.com>
2018-11-01 16:50:12 -0600

    password

--- web/week4/newClimber.php
+++ web/week4/newClimber.php
@@ -25,8 +25,8 @@ catch (PDOException $ex)
 $name = $_POST['climber'];
 $password = $_POST['password'];
 
-$stmt = $db->prepare("SELECT name FROM climber WHERE name = :name");
-$stmt->bindParam(':name', $name);
+$stmt = $db->prepare("SELECT name FROM climber WHERE name = :ame");
+//$stmt->bindParam(':name', $name);
 //$stmt->execute(array(':name' => $name));
 //$stmt->fetch(PDO::FETCH_ASSOC);
 if ($stmt == TRUE){
